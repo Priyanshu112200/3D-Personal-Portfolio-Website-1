dcc453b90948d03df39d4337ee0f31beaa225666
Priyanshu112200 <143899912+Priyanshu112200@users.noreply.github.com>
2023-10-06 20:52:23 +0530

    3D-Personal-Portfolio-Website-1
    MY FIRST 3D-PERSONAL-PORTFOLIO-WEBSITE ❤❤❤

---
+++ Data.js
@@ -0,0 +1,17 @@
+const servicesData = [
+    {
+        title: "Frontend Development",
+        category: "frontend",
+        description: ['Frontend ipsum dolor sit amet consectetur adipisicing elit. Beatae quaerat official maxime ullam. Distinctio voluptate officia voluptats unde debitis quia corporis doloribus labore vero? Soluta dolorum sapiente dolores libero accusantium.', 'Consectetur adipiscing elit. Autem tempora vitae magni sint hic non consectetur voluptatum accusamus, est dicta commodi, possimus laborum nam deserunt blanditiis ratione sit iste in, quisquam corporis quod voluptates voluptas natus. Optio doloremque, at nisi voluptas sapiente quidem expedita mollitia itaque tempora vero deleniti error, quod ipsam officiis dicta facere culpa reprehenderit provident animi. Architecto? ', 'Dolor sit amet consectetur  adipisicing  elit. Id voluptatum suscipit voluptatibus ex illo porro similique, dicta dignissimos facilis? Illo.']
+    },
+    {
+        title: "Backend Development",
+        category: "backend",
+        description: ['Backend ratione sit iste in, quisqam corporis quod voluptates voluptas natus. Optio doloremque, at nisi voluptas sapiente quidem  expedita mollitia itaque tempora vero deleniti error, quod ipsam officiis dicta facere culpa reprehenderit provident animi. Architecto?', 'Dolar sit amet consectetur adipisicing elit. Id voluptatum suscipit voluptatibus', 'Autem tempora vitae magni sint hic non consectetur voluptatum accusamus, est dicta commodi, possimus laborum nam  deserunt blanditiis ratione sit iste in, quisquam corporis quod voluptates voluptas natus. Optio doloremque, at nisi voluptas sapiente quidem expedita mollitia itaque tempora vero deleniti error, quod ipsam officiis dicta facere culpa reprehenderit provident animi. Architecto? Dolor sit amet consectetur  adipisicing elit. Id voluptam suscipit voluptatibus ex illo poro similique, dicta', 'Dolor sit amet consectetur adipisicing elit. Beatae quaerat officia maxime ullam. Distinctio voluptate officia voluptas unde debitis quia corporis doloribus labore vero? Soluta dolorum sapiente dolores libero']
+    },
+    {
+        title: "Fullstack Development",
+        category: "fullstack",
+        description: ['Fullstack Consectetur adipisicing elit. Beatae querat officia maxime ullam. Distinctio voluptate officia voluptas unde debitis quia corporis doloribus labore vero?', 'Quisquam corporis quod voluptates voluptas natus. Optio doloremque, at nisi voluptas sapiente quidem expedita mollitia itaque tempora vero deleniti error, quod ipsam officiis dicta facere culpa reprehenderit provident animi. Architecto? Dolor sit', 'Sit amet consectetur adipisicing elit. Beatae quaerat officia maxime ullam. Distinctio voluptate officia voluptas unde debitis quia corporis doloribus labore vero? Soluta dolorum sapiente dolores libero']
+    },
+]
